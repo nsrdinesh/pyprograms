b8a4275c91e516a3c15849be338679b9eeead6d1
nsrdinesh <dinesh.santhanaraman@gmail.com>
2019-02-12 05:11:41 +0530

    updates

---
+++ flows.py
@@ -0,0 +1,26 @@
+
+age = 13
+
+if age >= 18:
+    print ("Eligle for voting")
+elif age >= 13 and age < 18:
+    print ("Teenager")
+else:
+    print ("Child")
+
+print ("All Done!")
+
+
+name = ""
+
+if not name:
+    print ("Name is empty")
+
+
+for x in range(0,10,2):
+    print (x)
+
+def add(input1 , input2):
+    return input1 + input2
+
+print (add(10,20))
